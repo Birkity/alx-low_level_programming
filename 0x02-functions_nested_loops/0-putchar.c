8444662c2aa5a94fdd4e28af817a1df369e23a74
Birkity <lilybluestorm7gmail.com>
2022-03-15 07:44:02 -0700

    task 00

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,21 @@
-#include <stdio.h>
-int main(){
-print("_putchar\n");
-return 0;
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
