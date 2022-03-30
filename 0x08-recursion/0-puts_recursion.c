6996f19cdf706029f1c9d8f3092858219bdf83ea
Birkity <lilybluestorm7gmail.com>
2022-03-30 07:02:41 -0700

    task 000

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
 /**
-*main - check the code 
+*main - check the code
 * Return: Always 0.
 */
+void _puts_recursion(char *s){
+	printf("%c\n", s );
+	s++;
+	_puts_recursion(s);
+}
 
 int main(void){
  _puts_recursion("Puts with recursion");
