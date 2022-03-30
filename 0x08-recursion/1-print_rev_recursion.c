82f9c03da87d6a86989e4e9c74a110fab6629b78
Birkity <lilybluestorm7gmail.com>
2022-03-30 07:30:20 -0700

    task 01

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s){
+	if (s != '\n') {
+        _print_rev_recursion(s);
+        printf("%c", s);
+    }
+}
+int main(void)
+{
+    _print_rev_recursion("\nColton Walker");
+    return (0);
+}
