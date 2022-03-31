9f5d5a19470d89d54d94f66a97e7b53c3e1369e1
Birkity <lilybluestorm7gmail.com>
2022-03-30 23:41:03 -0700

    all tasks complete

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,18 +1,14 @@
 #include "main.h"
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _print_rev_recursion - function that prints a string in reverse.
+ * @s: string to recurse
+ * Return: output
  */
-void _print_rev_recursion(char *s){
-	if (s != '\n') {
-        _print_rev_recursion(s);
-        printf("%c", s);
-    }
-}
-int main(void)
+void _print_rev_recursion(char *s)
 {
-    _print_rev_recursion("\nColton Walker");
-    return (0);
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
