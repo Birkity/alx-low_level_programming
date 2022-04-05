e55f08aa4d6d9c52d08517e14a9ca880f2934cc8
Birkity <lilybluestorm7gmail.com>
2022-04-05 03:12:46 -0700

    task 00

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
 
-int main(int argc, char const *argv[])
+
+*/
+
+int main(int argc __attribute__((unused)), char const *argv[])
 {
-	int i;
-	for ( i = 0; i < argc; ++i)
-	{
-		printf("argv[%d] = %s\n", i, argv[i] );
-	}
+	
+		printf("%s\n",argv[0] );
+
 	return 0;
 }
