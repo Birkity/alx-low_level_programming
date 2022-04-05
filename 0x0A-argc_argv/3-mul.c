1328438b00f2ffea28aea925f0fd5136ab234bab
Birkity <lilybluestorm7gmail.com>
2022-04-05 06:56:44 -0700

    task 003

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,15 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int mul;
-	int num1;
-	int num2;
+	int mul, num1, num2;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 	mul = num1 * num2; 
