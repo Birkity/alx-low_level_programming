5273b956430721239bb51ecea50c9c2b4e87a2ee
Birkity <lilybluestorm7gmail.com>
2022-04-05 06:31:47 -0700

    task 02

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**print all arguments
+*Return -0
+*/
+
+int main(int argc, char const *argv[])
+{
+	int i;
+	for ( i = 0; i < argc; ++i)
+	{
+		printf("%s\n", argv[i] );
+		
+	}
+	return (0);
+}
