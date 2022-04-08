7e7ab5300cf04a6257dc8d4c18defc346358b9ca
Birkity <lilybluestorm7gmail.com>
2022-04-08 06:09:54 -0700

    task 01

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,28 +1,53 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdlib.h>
+#include "main.h"
 /**
- * create_array - creates an array of chars.
- * @size: size of the array.
- * @c: storaged char
- *
- * Return: pointer of an array of chars
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
  */
+int _strlen(char *string)
+{
+	int i;
 
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int i,j;
+	char *ptr;
+	int num, len, i, j;
 
-	if (s1 == NULL || s2 == NULL)
-	   return(NULL);
+	num = n;
 
-	s = malloc(n * sizeof(int));
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0)
+		return (NULL);
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
 
-	for (i = 0; s1 !='\0'; ++i);
-		s[i] = s1[i];
-	for (j = 0; j <= '\0'; ++j)
-	s[i+j] = s2[j];
+	len = _strlen(s1) + num + 1;
 
-     return(s);
-	
+	ptr = malloc(sizeof(*ptr) * len);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		ptr[i] = s1[i];
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
+	return (ptr);
 }
