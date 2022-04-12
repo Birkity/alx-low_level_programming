33dd7350d4ee7d46c80fcaf5937c95c2690ff49c
Birkity <lilybluestorm7gmail.com>
2022-04-12 11:30:52 -0700

    task 0003

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-/*
 
-
-
-*/
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
 
 int main()
 {
-	printf("%d\n"__FILE__);
+	printf("%s\n"__FILE__);
 	return 0;
 }
